8fd689d578ce7702e43295f953b5238a6456e03b
Jack <66899255+maomao79b@users.noreply.github.com>
2023-07-16 00:42:04 +0700

    saleHistory

---
+++ shopApi/Controllers/SaleHistoryController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using shopApi.Models;
+using System.Data;
+
+namespace shopApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SaleHistoryController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public SaleHistoryController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string sqlDataSource = _configuration.GetConnectionString("ShopAppcon");
+            DataTable table = new DataTable();
+            MySqlDataReader myReader;
+            string query = @"SELECT * FROM `sale_history`";
+
+            MySqlConnection mycon = new MySqlConnection(sqlDataSource);
+            mycon.Open();
+
+            MySqlCommand mySqlCommand = new MySqlCommand(query, mycon);
+            myReader = mySqlCommand.ExecuteReader();
+            table.Load(myReader);
+
+            myReader.Close();
+            mycon.Close();
+            return new JsonResult(table);
+        }
+
+        [HttpDelete]
+        public JsonResult Delete(int id)
+        {
+            string sqlDataSource = _configuration.GetConnectionString("ShopAppcon");
+            DataTable table = new DataTable();
+            MySqlDataReader myReader;
+            string query = $"DELETE FROM `sale_history` WHERE id = {id};";
+
+            MySqlConnection mycon = new MySqlConnection(sqlDataSource);
+            mycon.Open();
+
+            MySqlCommand mySqlCommand = new MySqlCommand(query, mycon);
+            myReader = mySqlCommand.ExecuteReader();
+            table.Load(myReader);
+
+            myReader.Close();
+            mycon.Close();
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(SaleHistory saleHistory)
+        {
+            string sqlDataSource = _configuration.GetConnectionString("ShopAppcon");
+
+            string query = @"INSERT INTO `sale_history` (`id`, `cid`, `total`, `product`) " +
+                            @"VALUES (@id,@cid,@total,@product)";
+
+            MySqlConnection mycon = new MySqlConnection(sqlDataSource);
+            mycon.Open();
+
+            MySqlCommand mySqlCommand = new MySqlCommand(query, mycon);
+            mySqlCommand.Parameters.AddWithValue("id", saleHistory.Id);
+            mySqlCommand.Parameters.AddWithValue("@cid", saleHistory.Cid);
+            mySqlCommand.Parameters.AddWithValue("@total", saleHistory.Total);
+            mySqlCommand.Parameters.AddWithValue("@product", saleHistory.Product);
+            mySqlCommand.ExecuteNonQuery();
+
+            mycon.Close();
+            return new JsonResult("Insert Successfully");
+        }
+    }
+}
---
+++ shopApi/Models/SaleHistory.cs
@@ -0,0 +1,10 @@
+﻿namespace shopApi.Models
+{
+    public class SaleHistory
+    {
+        public int Id { get; set; }
+        public string Cid { get; set; }
+        public Decimal Total { get; set; }
+        public string Product { get; set; }
+    }
+}
